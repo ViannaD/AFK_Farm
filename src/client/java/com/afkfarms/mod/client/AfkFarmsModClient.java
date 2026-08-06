770011eb4aefdfc38f63186b3994495ac547ba72
Vianna <112437736+ViannaD@users.noreply.github.com>
2026-08-05 23:45:02 -0300

    Add files via upload

--- src/client/java/com/afkfarms/mod/client/AfkFarmsModClient.java
+++ src/client/java/com/afkfarms/mod/client/AfkFarmsModClient.java
@@ -4,8 +4,12 @@ import com.afkfarms.mod.client.render.WheatFarmBlockEntityRenderer;
 import com.afkfarms.mod.client.render.WheatFarmModel;
 import com.afkfarms.mod.registry.ModBlockEntities;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.rendering.v1.BlockEntityRendererFactories;
 import net.fabricmc.fabric.api.client.rendering.v1.EntityModelLayerRegistry;
+// This is the vanilla class, not a Fabric API one. Fabric API used to provide a wrapper
+// (BlockEntityRendererRegistry) for this, but it was deprecated in favor of just calling the
+// vanilla register() method directly - Fabric's "transitive access widener" mechanism makes
+// this normally-private method callable from mod code without any wrapper class needed.
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
 
 public class AfkFarmsModClient implements ClientModInitializer {
     @Override
